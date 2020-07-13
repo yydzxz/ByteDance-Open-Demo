7cfe70ed1c2252269a7e93c531676f14307a72fb
杨一点 <>
2020-07-13 11:11:14 +0800

    update bytedance-open sdk version

--- src/main/java/com/yyd/bytedance/open/demo/config/ByteDanceOpenServiceConfiguaration.java
+++ src/main/java/com/yyd/bytedance/open/demo/config/ByteDanceOpenServiceConfiguaration.java
@@ -1,11 +1,11 @@
 package com.yyd.bytedance.open.demo.config;
 
-import com.yyd.common.redis.RedissonByteDanceRedisOps;
-import com.yyd.common.service.impl.RestTemplateByteDanceHttpRequestServiceImpl;
-import com.yyd.open.api.IByteDanceOpenService;
-import com.yyd.open.api.impl.ByteDanceOpenComponentServiceImpl;
-import com.yyd.open.api.impl.ByteDanceOpenInRedisConfigStorage;
-import com.yyd.open.api.impl.ByteDanceOpenServiceImpl;
+import com.github.yydzxz.common.redis.RedissonByteDanceRedisOps;
+import com.github.yydzxz.common.service.impl.RestTemplateByteDanceHttpRequestServiceImpl;
+import com.github.yydzxz.open.api.IByteDanceOpenService;
+import com.github.yydzxz.open.api.impl.ByteDanceOpenComponentServiceImpl;
+import com.github.yydzxz.open.api.impl.ByteDanceOpenInRedisConfigStorage;
+import com.github.yydzxz.open.api.impl.ByteDanceOpenServiceImpl;
 import org.redisson.api.RedissonClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
@@ -31,7 +31,7 @@ public class ByteDanceOpenServiceConfiguaration {
     public IByteDanceOpenService getIByteDanceOpenService(){
         ByteDanceOpenServiceImpl byteDanceOpenService = new ByteDanceOpenServiceImpl();
         byteDanceOpenService.setByteDanceHttpRequestService(new RestTemplateByteDanceHttpRequestServiceImpl(restTemplate));
-        RedissonByteDanceRedisOps  redissonByteDanceRedisOps = new RedissonByteDanceRedisOps(redissonClient);
+        RedissonByteDanceRedisOps redissonByteDanceRedisOps = new RedissonByteDanceRedisOps(redissonClient);
         byteDanceOpenService.setRedissonByteDanceRedisOps(redissonByteDanceRedisOps);
         byteDanceOpenService.setByteDanceOpenComponentService(new ByteDanceOpenComponentServiceImpl(byteDanceOpenService));
         ByteDanceOpenInRedisConfigStorage inRedisConfigStorage = new ByteDanceOpenInRedisConfigStorage(redissonByteDanceRedisOps, "yourprefix");
