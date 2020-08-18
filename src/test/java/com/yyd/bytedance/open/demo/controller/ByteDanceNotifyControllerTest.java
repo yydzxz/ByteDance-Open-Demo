68db9d1733036dd296f734803b9e1720739b448c
杨一点 <>
2020-08-18 13:49:24 +0800

    update sdk version

--- src/test/java/com/yyd/bytedance/open/demo/controller/ByteDanceNotifyControllerTest.java
+++ src/test/java/com/yyd/bytedance/open/demo/controller/ByteDanceNotifyControllerTest.java
@@ -1,11 +1,11 @@
 package com.yyd.bytedance.open.demo.controller;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.jupiter.api.Assertions.assertEquals;
 
 import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
